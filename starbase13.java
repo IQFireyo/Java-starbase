a087505521ec99d64311ba1beca9e79d79637c5b
IQFireyo <144281323+IQFireyo@users.noreply.github.com>
2024-01-17 17:35:40 -0700

    Add files via upload

---
+++ starbase13.java
@@ -0,0 +1,18 @@
+// get input from the user
+import java.util.Scanner;
+
+//class called starbase13,
+public class starbase13{
+    //create our main method
+    public static void mian(string[] args){
+        //Create a welcome output
+    System.out.println("\n\n\tWelcome to StarBase13");
+    //Greet the user
+    System.out.println("whatis your name?")
+    // Create a scnner object
+    String userName = "";
+    Scanner scanner = new Scanner(System.in);
+    userName = scanner.nextLine();
+    System.out.println( "Welcome to name, " + userName + " Hope you have a blast?")
+    }   
+}
